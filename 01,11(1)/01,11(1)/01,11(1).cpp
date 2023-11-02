6bd15728ccb752a9fa41b1559b1a30a9eca818a3
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2023-11-02 13:50:39 +0200

    Add files via upload

---
+++ 01,11(1)/01,11(1)/01,11(1).cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+void addColumn(int** matrix, int rows, int columns, int columnIndex, int* newColumn) {
+    if (columnIndex < 0 || columnIndex > columns) {
+        std::cout << "Неверный индекс столбца" << std::endl;
+        return;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = columns; j > columnIndex; j--) {
+            matrix[i][j] = matrix[i][j - 1];
+        }
+        matrix[i][columnIndex] = newColumn[i];
+    }
+}
+
+int main() {
+    int rows = 3;
+    int columns = 3;
+    int** matrix = new int* [rows];
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = new int[columns];
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            matrix[i][j] = i * columns + j + 1;
+        }
+    }
+
+    int newColumn[] = { 10, 11, 12 };
+
+    addColumn(matrix, rows, columns, 1, newColumn);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns + 1; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
+    return 0;
+}
---
+++ 01,11(2)/01,11(2)/01,11(2).cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+void deleteColumn(int** matrix, int& rows, int& columns, int columnIndex) {
+    if (columnIndex < 0 || columnIndex >= columns) {
+        std::cout << "Неверный индекс столбца" << std::endl;
+        return;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = columnIndex; j < columns - 1; j++) {
+            matrix[i][j] = matrix[i][j + 1];
+        }
+    }
+    for (int i = 0; i < rows; i++) {
+        int* tempRow = new int[columns - 1];
+        for (int j = 0; j < columns - 1; j++) {
+            tempRow[j] = matrix[i][j];
+        }
+        delete[] matrix[i];
+        matrix[i] = tempRow;
+    }
+    columns--;
+}
+
+int main() {
+    int rows = 3;
+    int columns = 3;
+    int** matrix = new int* [rows];
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = new int[columns];
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            matrix[i][j] = i * columns + j + 1;
+        }
+    }
+
+    deleteColumn(matrix, rows, columns, 1);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
+    return 0;
+}
